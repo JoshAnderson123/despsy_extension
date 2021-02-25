b70ee717a598da16ecc0d5d7f8479950c46e0da0
JoshAnderson123 <joshfanderson1@gmail.com>
2021-02-25 11:51:36 +0000

    updated css

--- background.js
+++ background.js
@@ -2,18 +2,18 @@ let active_tab_id = 0
 let injected = false
 
 // Perform an action whenever the user activates a tab
-chrome.tabs.onActivated.addListener(tab => {
-  // Get the current tab id
-  chrome.tabs.get(tab.tabId, currentTabInfo => {
-    active_tab_id = tab.tabId
-    // Check if it is a google url
-    if (/^https:\/\/www\.google\.com\/search/.test(currentTabInfo.url)) {
-      // Insert CSS and execute foreground script
-      chrome.tabs.insertCSS(null, { file: './mystyles.css' })
-      chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
-    }
-  })
-})
+// chrome.tabs.onActivated.addListener(tab => {
+//   // Get the current tab id
+//   chrome.tabs.get(tab.tabId, currentTabInfo => {
+//     active_tab_id = tab.tabId
+//     // Check if it is a google url
+//     if (/^https:\/\/www\.google\.com\/search/.test(currentTabInfo.url)) {
+//       // Insert CSS and execute foreground script
+//       chrome.tabs.insertCSS(null, { file: './ranking.css' })
+//       chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
+//     }
+//   })
+// })
 
 chrome.tabs.onUpdated.addListener(tabId => {
   // Get the current tab id
@@ -21,8 +21,9 @@ chrome.tabs.onUpdated.addListener(tabId => {
     // Check if it is a google url
     if (/^https:\/\/www\.google\.com\/search/.test(currentTabInfo.url)) {
       // Insert CSS and execute foreground script
-      chrome.tabs.insertCSS(null, { file: './mystyles.css' })
-      chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
+      if (!injected) chrome.tabs.insertCSS(null, { file: './ranking.css' })
+      // chrome.tabs.insertCSS(null, { file: './popup.css' })  
+      chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('run foreground'))
     }
   })
 })
